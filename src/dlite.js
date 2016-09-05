9a72bd3b7744ce237b8ca2380d2403a74abd8aba
Guy Waldman <guywald1@gmail.com>
2016-09-05 03:40:34 +0300

    Add CSS and custom

--- src/dlite.js
+++ src/dlite.js
@@ -1,64 +1,78 @@
-var _$ = function(param, settings) {
+var $ = function(param, settings) {
     return {
 
         // properties
         //
         // IDEA: use isNode() or isElement()
-        el: (function(){
-          if (settings === true || settings.node)
-          return param;
-          return document.querySelector(param);
+        el: (function() {
+            if (settings === true || typeof settings === 'object' && settings.hasOwnProperty(node) && settings.node)
+                return param;
+            return document.querySelector(param);
         }()),
 
         // methods
         //
-        _$: function(param) {
-          return new _$(param);
+        $: function(param) {
+            return new $(param);
         },
-        addClass: function(pClass) {
+        addClass: function(classToAdd) {
             if (this.el.classList)
-                this.el.classList.add(pClass);
-            else if (!this.el.hasClass(pClass)) this.el.className += " " + pClass;
+                this.el.classList.add(classToAdd);
+            else if (!this.el.hasClass(classToAdd)) this.el.className += " " + classToAdd;
         },
-        removeClass: function(pClass) {
+        removeClass: function(classToRemove) {
             if (this.el.classList)
-                this.el.classList.remove(pClass);
-            else if (this.hasClass(pClass)) {
-                var reg = new RegExp('(\\s|^)' + pClass + '(\\s|$)');
+                this.el.classList.remove(classToRemove);
+            else if (this.hasClass(classToRemove)) {
+                var reg = new RegExp('(\\s|^)' + classToRemove + '(\\s|$)');
                 this.el.className = this.el.className.replace(reg, ' ');
             }
         },
-        toggleClass: function(pClass) {
+        toggleClass: function(classToToggle) {
             var classes = this.el.className;
-            var regex = new RegExp('\\b' + pClass + '\\b');
+            var regex = new RegExp('\\b' + classToToggle + '\\b');
             var hasOne = classes.match(regex);
-            pClass = pClass.replace(/\s+/g, '');
+            classToToggle = classToToggle.replace(/\s+/g, '');
             if (hasOne)
                 this.el.className = classes.replace(regex, '');
             else
-                this.el.className = classes + pClass;
+                this.el.className = classes + classToToggle;
         },
-        hasClass: function(pClass) {
+        hasClass: function(classToCheck) {
             if (el.classList)
-                return el.classList.contains(pClass);
+                return el.classList.contains(classToCheck);
             else
-                return !!el.className.match(new RegExp('(\\s|^)' + pClass + '(\\s|$)'));
+                return !!el.className.match(new RegExp('(\\s|^)' + classToCheck + '(\\s|$)'));
         },
         addEventListener: function() {
-            EventTarget.prototype.addEventListener.apply(this.el, arguments);
+            return EventTarget.prototype.addEventListener.apply(this.el, arguments);
         },
         removeEventListener: function() {
-            EventTarget.prototype.removeEventListener.apply(this.el, arguments);
+            return EventTarget.prototype.removeEventListener.apply(this.el, arguments);
         },
-        dispatchEvent: function(){
-          EventTarget.prototype.dispatchEvent.apply(this.el, arguments);
+        dispatchEvent: function() {
+            return EventTarget.prototype.dispatchEvent.apply(this.el, arguments);
         },
-        children: function(){
-          var children = this.el.children;
-          var res = [];
-          for (var i=0; i<children.length; i++)
-            res.push(new _$(children[i]), true);
-          return res;
+        children: function() {
+            var children = this.el.children;
+            var res = [];
+            for (var i = 0; i < children.length; i++)
+                res.push(new $(children[i]), true);
+            return res;
+        },
+        css: function(styles) {
+            if (typeof styles !== 'object') return;
+            for (var property in styles) {
+                if (typeof property === 'number' || typeof property === 'string') {
+                    if (this.el.style[property] !== undefined) {
+                        this.el.style[property] = String(styles[property]);
+                    }
+                }
+            }
+        },
+        custom: function(cb) {
+            if (!Element.prototype.cb) return;
+            return Element.prototype.cb.apply(this.el, cb);
         }
     };
 };
